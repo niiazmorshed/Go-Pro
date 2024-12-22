071ee56fc2d1a0a166b16a0334ffcaeb987a6c03
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-12-22 18:19:41 +0600

    I write the home and roots agaian

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <h1>Welcome to react boiss</h1>
+      <h2>Lets build upp againnnnn</h2>
+      <h2>Lets make it togetrher</h2>
     </>
   )
 }
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Roots from "../Layout/Roots";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Roots></Roots>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
